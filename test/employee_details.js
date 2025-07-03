4c4fe85944dae1be855c0f5a7787afde599dcdab
TanishVenpure <Tanishvenpure@gmail.com>
2025-07-03 14:16:52 +0530

    Update employee_details.js

--- test/employee_details.js
+++ test/employee_details.js
@@ -1,8 +1,8 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-      { id: 4, name: 'Matt Henry', age: 27, department: 'Marketing', salary: 50000},
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:'JavaScript'},
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization:'Python' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization:'Java' },
+      { id: 4, name: 'Matt Henry', age: 27, department: 'Marketing', salary: 50000, specialization:'C++'},
    ];
  
 function displayEmployees() {
@@ -22,6 +22,30 @@ function displayHREmployees() {
       document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
 }   
 
+function findEmployeeById(employeeId) {
+      const foundEmployee = employees.find(employee => employee.id === employeeId);
+      if (foundEmployee) {
+      document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+      }
+      else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+       }
+   }
+
+function findSpecialization(employeespec) {
+    const foundEmployee = employees.find(employee => 
+        employee.specialization.toLowerCase() === employeespec.toLowerCase()
+      );
+      
+      if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML =
+          `<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary} - ${foundEmployee.specialization}</p>`;
+      } else {
+        document.getElementById('employeesDetails').innerHTML = 'No employee found with this specialization';
+      }
+    
+}
+  
 
 
   
